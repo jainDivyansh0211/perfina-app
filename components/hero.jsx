19097b042f6598f5e770cf847b3054c71802dc49
jainDivyansh0211 <div0211jain@gmail.com>
2025-06-22 22:23:17 +0530

    all changes done good go for deployment

--- components/hero.jsx
+++ components/hero.jsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
   return (
     <div className='pb-20 px-4'>
         <div className='container mx-auto text-center '>
-            <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text'>Manage your Finance <br/> with Intelligence </h1>
+            <h1 className='text-5xl md:text-8xl lg:text-[105px] bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text'>Manage your Finance <br/> with Intelligence </h1>
         <p className='text-xl text-gray-600 mb-8 max-w-2xl mx-auto'>
             An AI-powered financial management platform that helps you track,
             analyze, and optimize your spending with real-time insights.
