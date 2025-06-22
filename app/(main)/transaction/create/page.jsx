be7e184177581b690a73bfb0d8649e27839694fe
jainDivyansh0211 <div0211jain@gmail.com>
2025-06-22 20:40:33 +0530

    more files added

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import { AddTransactionForm } from "../_components/transaction-form";
+import { getTransaction } from "@/actions/transaction";
 
-const page = () => {
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
+  const editId = searchParams?.edit;
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
+
   return (
-    <div>
-      
+    <div className="w-full min-h-screen flex flex-col items-center">
+      <div className="w-full max-w-3xl px-5">
+        <div className="flex justify-center md:justify-normal mb-8">
+          <h1 className="text-6xl bg-gradient-to-br from-blue-600 to-purple-600
+        mb-4 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text">
+            Add Transaction
+          </h1>
+        </div>
+        <AddTransactionForm
+          accounts={accounts}
+          categories={defaultCategories}
+          editMode={!!editId}
+          initialData={initialData}
+        />
+      </div>
     </div>
-  )
+  );
 }
-
-export default page
